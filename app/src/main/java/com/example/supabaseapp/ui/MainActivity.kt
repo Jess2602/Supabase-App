46272668a60d76e88df8235210eb38af25011d03
Jess2602 <85699034+Jess2602@users.noreply.github.com>
2023-10-28 23:03:01 -0600

    Supabase Connection Succesful
    The supabase connection was succesful but there are issues with the data, it comes back empty.

--- app/src/main/java/com/example/supabaseapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/supabaseapp/ui/MainActivity.kt
@@ -2,24 +2,23 @@ package com.example.supabaseapp.ui
 
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.example.supabaseapp.R
-import io.github.jan.supabase.SupabaseClient
-import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.gotrue.GoTrue
-import io.github.jan.supabase.postgrest.Postgrest
-import io.github.jan.supabase.postgrest.postgrest
-import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request.*
+import okhttp3.Response
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getData()
+//        getData()
+        fetchSupabaseData()
 
         setContentView(R.layout.main_activity)
 
@@ -30,27 +29,38 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getData() {
-        lifecycleScope.launch {
-            val client = getClient()
-            val supabaseResponse = client.postgrest["users"].select()
-            val data = supabaseResponse.decodeList<User>()
-            Log.e("supabase", data.toString())
-        }
+    private fun fetchSupabaseData() {
+        val supabaseUrl = "https://orsdaquwteitkgppthww.supabase.co"
+        val supabaseKey =
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yc2RhcXV3dGVpdGtncHB0aHd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc5NDYyOTIsImV4cCI6MjAxMzUyMjI5Mn0.zuqYT50yUvFMfcxYxUvSg4NS1bO_hdBifJPsIzP_cYQ"
 
+        val client = OkHttpClient()
+        val request = Builder()
+            .url("$supabaseUrl/rest/v1/users")
+            .header("apikey", supabaseKey)
+            .get()
+            .build()
 
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Error al obtener datos de Supabase: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val responseData = response.body?.string()
+                    println("Datos de Supabase dddd: $responseData")
+
+                } else {
+                    println("Error al obtener datos de Supabase: ${response.code}")
+                }
+            }
+        })
     }
 
-    private fun getClient() : SupabaseClient {
-        return createSupabaseClient(
-            supabaseUrl = "https://orsdaquwteitkgppthww.supabase.co",
-            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yc2RhcXV3dGVpdGtncHB0aHd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc5NDYyOTIsImV4cCI6MjAxMzUyMjI5Mn0.zuqYT50yUvFMfcxYxUvSg4NS1bO_hdBifJPsIzP_cYQ"
-        ) {
-            install(GoTrue)
-            install(Postgrest)
-        }
-    }
+
 }
+
 @Serializable
 data class User(
     val id: Int = 0,
