dcf7c30950c82ccf982638198e747f0021968d11
Jess2602 <85699034+Jess2602@users.noreply.github.com>
2023-10-30 17:43:55 -0600

    Add function added

--- app/src/main/java/com/example/supabaseapp/ui/AddDiscActivity.kt
+++ app/src/main/java/com/example/supabaseapp/ui/AddDiscActivity.kt
@@ -1,24 +1,24 @@
 package com.example.supabaseapp.ui
 
+
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.supabaseapp.databinding.AddDiscActivityBinding
-import com.example.supabaseapp.databinding.HomeActivityBinding
-import com.example.supabaseapp.databinding.MainActivityBinding
-import com.example.supabaseapp.ui.theme.SupabaseAppTheme
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.FormBody
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.Request
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import java.io.IOException
 
 class AddDiscActivity : AppCompatActivity() {
-
     private lateinit var binding: AddDiscActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,10 +26,56 @@ class AddDiscActivity : AppCompatActivity() {
         binding = AddDiscActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Handle button click to add data to Supabase
         binding.addDiscToRecyclerButton.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+            val discName = binding.discNameEditText.text.toString()
+            val discYear = binding.discYearEditText.text.toString()
+            val discArtist = binding.discArtistEditText.text.toString()
+
+            addDataToSupabase(discName, discYear, discArtist)
+
+
         }
     }
+
+    private fun addDataToSupabase(title: String, year: String, artist: String) {
+        val supabaseUrl = "https://orsdaquwteitkgppthww.supabase.co"
+        val supabaseKey =
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yc2RhcXV3dGVpdGtncHB0aHd3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5Nzk0NjI5MiwiZXhwIjoyMDEzNTIyMjkyfQ.C-Yk_PVltRV4upfWNQ0hAgTxnP8MMWTCOPMTgApeaww"
+
+        val client = OkHttpClient()
+        val requestBody = FormBody.Builder()
+            .add("discName", title)
+            .add("discYear", year)
+            .add("discArtist", artist)
+            .build()
+
+        val request = Request.Builder()
+            .url("$supabaseUrl/rest/v1/discs") // Reemplaza "your_table_name" por el nombre de tu tabla
+            .header("apikey", supabaseKey)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Error al insertar datos en Supabase: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    println("Datos insertados en Supabase exitosamente")
+                    val intent = Intent(this@AddDiscActivity, HomeActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    println("Error al insertar datos en Supabase: ${response.code}")
+                }
+            }
+        })
+    }
+
+
 }
+
+
+
 
